af8dff5d4e9c4cf5ad0300c928f10c43f236e651
murat1994 <gaday1037@gmail.com>
2020-10-13 16:41:31 +0300

    Simple Conditionals

---
+++ script.js
@@ -0,0 +1,20 @@
+//Grade Students
+let score = prompt(" Lütfen notunuzu giriniz. Not 0 ile 100 arası");
+
+
+if ( score >= 80 && score <= 100) {
+    alert("A aldınız")
+} else if (score >70 && score <= 89) {
+    alert("B aldınız")
+} else if (score >60 && score <= 69) {
+    alert("C aldınız")
+} else if (score >50 && score <= 59) {
+    alert("D aldınız")
+} else if (score >0 && score <= 49) {
+    alert("F aldınız")
+} else {
+    alert("Not 0 ile 100 arası")
+}
+
+
+//Check Season 
